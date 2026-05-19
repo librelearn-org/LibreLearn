bc2ce9101bb1df9626f1ba03f4717b6da1894b61
Siemvk <100954549+siemvk@users.noreply.github.com>
2026-05-19 23:01:01 +0200

    gh desktop plz

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ["fsevents", "lightningcss"],
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
