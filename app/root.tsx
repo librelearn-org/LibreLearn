3104181dd5241f9bafdeb06ada9d1cedcf2a27a0
Siemvk <100954549+siemvk@users.noreply.github.com>
2026-05-21 22:23:51 +0200

    ???

--- app/root.tsx
+++ app/root.tsx
@@ -43,6 +43,8 @@ export function meta({ }: Route.MetaArgs) {
     { title: "Librelearn" },
     { name: "description", content: "Librelearn is een opensource alternative voor WRTS." },
     { property: "og:image", content: "https://github.com/librelearn-org/LibreLearn/blob/main/public/logos/LiL-VT-LONG-ICON-WITHBG.png?raw=true" },
+    { property: "og:title", content: "Librelearn" },
+    { property: "og:description", content: "Librelearn is een opensource alternative voor WRTS en andere platforms." },
   ];
 }
 
