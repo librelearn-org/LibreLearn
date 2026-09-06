c8680afc433a552f4268089174186894cd889972
doodles172 <eamon@vantern.org>
2026-09-06 02:25:45 +0200

    feat(dash): good morning and good evening, welcoming

--- app/routes/app/home.tsx
+++ app/routes/app/home.tsx
@@ -5,12 +5,16 @@ import { Logo } from "~/components/Logo";
 export default function Home() {
     const user = useOutletContext<any>();
     const { t } = useTranslation();
+    const firstName = user?.name?.split(" ")[0] ?? "";
+    const greeting = new Date().getHours() < 18
+        ? t("startPage:greetingMorning", { name: firstName })
+        : t("startPage:greetingEvening", { name: firstName });
 
     return (
         <div>
             <div>
                 <div>
-                    <h1>{t("startPage:welcome")} <Logo style={{ height: "1em", margin: "0", padding: "-10em" }} /></h1>
+                    <h1>{greeting} <Logo style={{ height: "1em", margin: "0", padding: "-10em" }} /></h1>
 
                     <p>{t("startPage:description")}</p>
 
