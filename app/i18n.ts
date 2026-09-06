c860c26ecb5b70fb0b06d73f480bd438d69f4fca
doodles172 <eamon@vantern.org>
2026-09-06 02:32:45 +0200

    feat(i18n): detect language automatically

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,27 +1,36 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import nlTranslation from "./locales/nl.json";
 import enTranslation from "./locales/en.json";
-import config from "./utils/config";
 
-export function initI18n(lang: string = config.lang) {
-  const lng = lang || config.lang;
-
-  if (!i18n.isInitialized) {
-    i18n.use(initReactI18next).init({
-      lng,
-      fallbackLng: "nl",
-      resources: {
-        nl: nlTranslation,
-        en: enTranslation,
-      },
-      interpolation: { escapeValue: false },
-      react: { useSuspense: false },
-      initImmediate: false,
-    });
-  } else {
-    i18n.changeLanguage(lng);
+export function initI18n(lang?: string) {
+  if (i18n.isInitialized) {
+    if (lang) i18n.changeLanguage(lang);
+    return i18n;
   }
+
+  let instance = i18n.use(initReactI18next);
+  if (!lang) {
+    instance = instance.use(LanguageDetector);
+  }
+
+  instance.init({
+    ...(lang ? { lng: lang } : {}),
+    fallbackLng: "nl",
+    supportedLngs: ["nl", "en"],
+    resources: {
+      nl: nlTranslation,
+      en: enTranslation,
+    },
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false },
+    initImmediate: false,
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  });
 
   return i18n;
 }
